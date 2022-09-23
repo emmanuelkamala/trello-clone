1336a67b14cea16e6ae88c0b0bb3db20496d77ea
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-09-23 09:01:46 +0300

    Add item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_055647) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_060045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_055647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "list_id", null: false
+    t.string "title"
+    t.string "class_list"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_items_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -44,5 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_055647) do
   end
 
   add_foreign_key "boards", "users"
+  add_foreign_key "items", "lists"
   add_foreign_key "lists", "boards"
 end
